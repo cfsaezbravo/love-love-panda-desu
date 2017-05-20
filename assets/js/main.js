23ad4a96147068c7b36f19af9980f181c919e19c
Camila Sáez <cfsaezbravo@gmail.com>
2017-05-20 19:45:47 -0400

    FINISH

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
 
+var contenido = document.getElementById("todas-img");
+pictures.forEach(function(e){
+var newImage = document.createElement("img");
+newImage.setAttribute("class", "image");
+newImage.setAttribute("src", e);
+contenido.appendChild(newImage);
+
+})
+
 
